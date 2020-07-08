91b78b100bb6acc598d67f32f2146d73288ed6be
Max <67057312+max-tuda@users.noreply.github.com>
2020-07-08 12:40:58 +0200

    merged FusedLocationProviderClient branch

--- app/src/main/java/com/g15/smarthelper/Constants.java
+++ app/src/main/java/com/g15/smarthelper/Constants.java
@@ -2,8 +2,17 @@ package com.g15.smarthelper;
 
 public class Constants {
 
+    // Activity Recognition
     public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
     public static final long DETECTION_INTERVAL_IN_MILLISECONDS = 5 * 1000; // 5 seconds
     public static final int CONFIDENCE = 70;
+
+    // Location
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+    public static final String PACKAGE_NAME = "com.g15.smarthelper";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
 
 }
